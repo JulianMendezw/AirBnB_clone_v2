2f96567e3e52619cc972f3fea7a3c65ba000a3ad
Julian <julianlx@hotmail.com>
2021-04-15 22:33:57 -0500

    adding do clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -65,15 +65,22 @@ def deploy():
 
 
 def do_clean(number=0):
-    """ Do clean """
-    if (int(number) >= 0):
-        if (int(number) < 2):
-            number = 2
-        else:
-            number = int(number)+1
-        r_path = "/data/web_static/releases/"
-        l_path = "./versions/"
-        with cd(r_path):
-            run("ls -1t | tail -n +{} | xargs rm -rf --".format(number))
-        with lcd(l_path):
-            local("ls -1t | tail -n +{} | xargs rm -rf --".format(number))
+    """ Deletes out-of-date archives """
+
+    try:
+        number = int(number)
+    except BaseException:
+        return None
+
+    if number < 0:
+        return None
+
+    number = 2 if (number == 0 or number == 1) else (number + 1)
+
+    with lcd("./versions"):
+        local('ls -t | tail -n +{:d} | xargs rm -rf --'.
+              format(number))
+
+    with cd("/data/web_static/releases"):
+        run('ls -t | tail -n +{:d} | xargs rm -rf --'.
+            format(number))
