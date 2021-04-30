d26215a77e28edc986135b77a1b264f5671fbe9d
Julian <julianlx@hotmail.com>
2021-04-30 16:50:14 -0500

    return a text

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from flask import Flask
+
+# Create a Flask constructor.
+# It takes name of the current module as the argument
+app = Flask(__name__)
+
+# Create a route decorator to tell the application, which URL should be
+# called for the #described function and define the function.
+
+
+@app.route('/', strict_slashes=False)
+def web_flask():
+    """ Create a route decorator to tell the application, which URL should be
+        called for the #described function and define the function. """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """ Return HBNB text """
+    return "HBNB!"
+
+
+# Create the main driver function
+if __name__ == '__main__':
+    # call the run method
+    app.run(host="0.0.0.0", port="5000")
