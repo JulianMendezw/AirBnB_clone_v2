03a6d1b6aac2eb971c56d1a8242e5f1e1b8ca1bf
Julian <julianlx@hotmail.com>
2021-04-30 18:27:50 -0500

    rendering a template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+from flask import Flask
+
+# Create a Flask constructor.
+# It takes name of the current module as the argument
+app = Flask(__name__)
+
+# Create a route decorator to tell the application, which URL should be
+# called for the #described function and define the function.
+
+
+@app.route('/', strict_slashes=False)
+def web_flask():
+    """ Create a route decorator to tell the application, which URL should be
+        called for the #described function and define the function. """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """ Return HBNB text """
+    return "HBNB!"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_user_text(text):
+    """ Take a argument and return it """
+    text = text.replace('_', ' ')
+    return "C " + text
+
+
+@app.route('/python/', defaults={'text': 'is fun'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_user_text_2(text):
+    """ Take a argument and return it """
+    text = text.replace('_', ' ')
+    return "Python " + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def show_user_number(n):
+    """Show a number if the argument is a number """
+    return ("%d is a number" % n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def render_template_2(n=None):
+    """ render a html if n is number """
+    return render_template('5-number.html', n=n)
+
+
+# Create the main driver function
+if __name__ == '__main__':
+    # call the run method
+    app.run(host="0.0.0.0", port="5000")
