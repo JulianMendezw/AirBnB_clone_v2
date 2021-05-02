4dec44ebec486e9ac9beedf92c64a33e38b06efc
Julian Mendez <julianlx@hotmail.com>
2021-05-02 22:40:10 +0000

    adding peer method

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,11 +18,8 @@ def teardown_appcontext_handle(error):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ displays a HTML page with a list of states """
-    states_dict = storage.all(State)
-    states = {}
-    for key, value in states_dict.items():
-        states[value.id] = value.name
-
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
 
 
