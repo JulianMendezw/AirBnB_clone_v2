116cb7ce7a5050ff997d246a3256c0c62d9a1854
Ubuntu <stackez@stackez.zqme0knehrre3dg3nkxxgyoaeh.jx.internal.cloudapp.net>
2021-05-02 05:05:19 +0000

    fixing teardown context

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_appcontext_handle():
+def teardown_appcontext_handle(error):
     """" remove the current SQLAlchemy Session """
     storage.close()
 
